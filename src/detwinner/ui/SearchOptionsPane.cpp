6c51782fd10015dec9a4e8e49c60fbcd1aa18edd
neatdecisions <opensource@neatdecisions.com>
2019-09-01 13:00:23 +0200

    Force update_places

--- src/detwinner/ui/SearchOptionsPane.cpp
+++ src/detwinner/ui/SearchOptionsPane.cpp
@@ -38,6 +38,8 @@ SearchOptionsPane::SearchOptionsPane() :
 	m_places.set_show_trash(false);
 	m_places.set_show_enter_location(false);
 	m_places.set_show_other_locations(false);
+	// set to true and then to false - to ensure that update_places is executed
+	m_places.set_show_recent(true);
 	m_places.set_show_recent(false);
 	m_places.signal_open_location().connect(sigc::mem_fun(*this, &SearchOptionsPane::on_open_location));
 
